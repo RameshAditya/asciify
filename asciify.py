dc718db5edc5c18d4a0dbdd44e26a76de489dd3b
Warty <ItzWarty@gmail.com>
2018-08-22 22:35:13 -0700

    Refactor char_lut

--- asciify.py
+++ asciify.py
@@ -1,15 +1,13 @@
 import sys
 from PIL import Image
 
-ASCII_CHARS = "@#S%?*+;:,."
-
-def to_ascii(original, output_width):
+def to_ascii(original, output_width, char_lut):
     (original_width, original_height) = original.size
     output_height = output_width * original_height // original_width
     resized_grayscale = original.resize((output_width, output_height)).convert('L')
-    chars = [ASCII_CHARS[lum * len(ASCII_CHARS) // 256] for lum in list(resized_grayscale.getdata())]
+    chars = [char_lut[lum * len(char_lut) // 256] for lum in list(resized_grayscale.getdata())]
     rows = [[chars[y * output_width + x] for x in range(output_width)] for y in range(output_height)]
     return '\r\n'.join([''.join(row) for row in rows])
 
 if __name__ == '__main__':
-    print(to_ascii(Image.open(sys.argv[1]), 100))
+    print(to_ascii(Image.open(sys.argv[1]), 100, "@#S%?*+;:,."))
