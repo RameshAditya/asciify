63297a7fae5cc1c6e15f7ae1b6355e1d1d9ead2f
Warty <ItzWarty@gmail.com>
2018-08-22 22:31:53 -0700

    main => to_ascii

--- asciify.py
+++ asciify.py
@@ -3,15 +3,13 @@ from PIL import Image
 
 ASCII_CHARS = "@#S%?*+;:,."
 
-def main(path, output_width):
-    image = Image.open(path)
-    (old_width, old_height) = image.size
-    output_height = output_width * old_height // old_width
-    resized_grayscale = image.resize((output_width, output_height)).convert('L')
+def to_ascii(original, output_width):
+    (original_width, original_height) = original.size
+    output_height = output_width * original_height // original_width
+    resized_grayscale = original.resize((output_width, output_height)).convert('L')
     chars = [ASCII_CHARS[lum * len(ASCII_CHARS) // 256] for lum in list(resized_grayscale.getdata())]
     rows = [[chars[y * output_width + x] for x in range(output_width)] for y in range(output_height)]
-    print('\r\n'.join([''.join(row) for row in rows]))
+    return '\r\n'.join([''.join(row) for row in rows])
 
 if __name__ == '__main__':
-    path = sys.argv[1]
-    main(path, 100)
+    print(to_ascii(Image.open(sys.argv[1]), 100))
