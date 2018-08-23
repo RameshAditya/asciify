7c87bcff3844060b04b1662d6730b634fa67abe7
Warty <ItzWarty@gmail.com>
2018-08-22 22:52:14 -0700

    Improve group by row

--- asciify.py
+++ asciify.py
@@ -6,7 +6,7 @@ def to_ascii(original_image, output_width=100, char_lut="@#S%?*+;:,."):
     output_height = output_width * original_height // original_width
     resized_grayscale_image = original_image.resize((output_width, output_height)).convert('L')
     chars = [char_lut[lum * len(char_lut) // 256] for lum in list(resized_grayscale_image.getdata())]
-    rows = [[chars[y * output_width + x] for x in range(output_width)] for y in range(output_height)]
+    rows = [chars[i:i+output_width] for i in range(0, len(chars), output_width)]
     return '\r\n'.join([''.join(row) for row in rows])
 
 if __name__ == '__main__':
