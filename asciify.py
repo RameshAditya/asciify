c673a5c2cb3d3d100d9051b24210e46430a6b37d
Wassim Dhif <wassimdhif@gmail.com>
2018-10-14 20:58:44 +0200

    Make asciify comply with PEP8

--- asciify.py
+++ asciify.py
@@ -1,42 +1,52 @@
+#!/usr/bin/env python
+
+import sys
+import urllib.request
+
 from PIL import Image
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
+ASCII_CHARS = ['.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
 ASCII_CHARS = ASCII_CHARS[::-1]
 
-'''
-method resize():
-    - takes as parameters the image, and the final width
-    - resizes the image into the final width while maintaining aspect ratio
-'''
+
 def resize(image, new_width=100):
+    """
+    method resize():
+        - takes as parameters the image, and the final width
+        - resizes the image into the final width while maintaining aspect ratio
+    """
     (old_width, old_height) = image.size
-    aspect_ratio = float(old_height)/float(old_width)
+    aspect_ratio = float(old_height) / float(old_width)
     new_height = int(aspect_ratio * new_width)
     new_dim = (new_width, new_height)
     new_image = image.resize(new_dim)
     return new_image
-'''
-method grayscalify():
-    - takes an image as a parameter
-    - returns the grayscale version of image
-'''
+
+
 def grayscalify(image):
+    """
+    method grayscalify():
+        - takes an image as a parameter
+        - returns the grayscale version of image
+    """
     return image.convert('L')
 
-'''
-method modify():
-    - replaces every pixel with a character whose intensity is similar
-'''
+
 def modify(image, buckets=25):
+    """
+    method modify():
+        - replaces every pixel with a character whose intensity is similar
+    """
     initial_pixels = list(image.getdata())
-    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
+    new_pixels = [ASCII_CHARS[pixel_value // buckets] for pixel_value in initial_pixels]
     return ''.join(new_pixels)
 
-'''
-method do():
-    - does all the work by calling all the above functions
-'''
+
 def do(image, new_width=100):
+    """
+    method do():
+        - does all the work by calling all the above functions
+    """
     image = resize(image)
     image = grayscalify(image)
 
@@ -44,23 +54,23 @@ def do(image, new_width=100):
     len_pixels = len(pixels)
 
     # Construct the image from the character list
-    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
+    new_image = [pixels[index:index + new_width] for index in range(0, len_pixels, new_width)]
 
     return '\n'.join(new_image)
 
-'''
-method runner():
-    - takes as parameter the image path and runs the above code
-    - handles exceptions as well
-    - provides alternative output options
-'''
+
 def runner(path):
-    image = None
+    """
+    method runner():
+        - takes as parameter the image path and runs the above code
+        - handles exceptions as well
+        - provides alternative output options
+    """
     try:
         image = Image.open(path)
     except Exception:
-        print("Unable to find image in",path)
-        #print(e)
+        print("Unable to find image in", path)
+        # print(e)
         return
     image = do(image)
 
@@ -71,21 +81,25 @@ def runner(path):
     # Note: This text file will be created by default under
     #       the same directory as this python file,
     #       NOT in the directory from where the image is pulled.
-    f = open('img.txt','w')
+    f = open('img.txt', 'w')
     f.write(image)
     f.close()
 
-'''
-method main():
-    - reads input from console
-    - profit
-'''
-if __name__ == '__main__':
-    import sys
-    import urllib.request
+
+def main():
+    """
+    method main():
+        - reads input from console
+        - profit
+    """
+
     if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
         urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
         path = "asciify.jpg"
     else:
         path = sys.argv[1]
     runner(path)
+
+
+if __name__ == '__main__':
+    main()
