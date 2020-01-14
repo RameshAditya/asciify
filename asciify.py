6f279a846ca21624df3e995857ffc4f8907748d2
Carson <jackparadise520a@gmail.com>
2020-01-14 10:16:28 +0800

    (Make it conform to the naming convention of PEP8.)

--- asciify.py
+++ asciify.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
+ASCII_CHARS = ['.', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
 ASCII_CHARS = ASCII_CHARS[::-1]
 
 '''
@@ -8,45 +8,58 @@ method resize():
     - takes as parameters the image, and the final width
     - resizes the image into the final width while maintaining aspect ratio
 '''
+
+
 def resize(image, new_width=100):
-    (old_width, old_height) = image.size
-    aspect_ratio = float(old_height)/float(old_width)
+    old_width, old_height = image.size
+    aspect_ratio = float(old_height) / float(old_width)
     new_height = int(aspect_ratio * new_width)
-    new_dim = (new_width, new_height)
+    new_dim = new_width, new_height
     new_image = image.resize(new_dim)
     return new_image
+
+
 '''
-method grayscalify():
+method gray_scalify():
     - takes an image as a parameter
     - returns the grayscale version of image
 '''
-def grayscalify(image):
+
+
+def gray_scarify(image):
     return image.convert('L')
+
 
 '''
 method modify():
     - replaces every pixel with a character whose intensity is similar
 '''
+
+
 def modify(image, buckets=25):
     initial_pixels = list(image.getdata())
-    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
+    new_pixels = [ASCII_CHARS[pixel_value // buckets] for pixel_value in initial_pixels]
     return ''.join(new_pixels)
+
 
 '''
 method do():
     - does all the work by calling all the above functions
 '''
+
+
 def do(image, new_width=100):
     image = resize(image)
-    image = grayscalify(image)
+    image = gray_scarify(image)
 
     pixels = modify(image)
     len_pixels = len(pixels)
 
     # Construct the image from the character list
-    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
+    new_image = [pixels[index:index + new_width] for index in range(0, len_pixels, new_width)]
 
     return '\n'.join(new_image)
+
 
 '''
 method runner():
@@ -54,13 +67,13 @@ method runner():
     - handles exceptions as well
     - provides alternative output options
 '''
+
+
 def runner(path):
-    image = None
     try:
         image = Image.open(path)
-    except Exception:
-        print("Unable to find image in",path)
-        #print(e)
+    except FileNotFoundError as e:
+        print(f"Unable to find image in. {str(e)}")
         return
     image = do(image)
 
@@ -71,9 +84,10 @@ def runner(path):
     # Note: This text file will be created by default under
     #       the same directory as this python file,
     #       NOT in the directory from where the image is pulled.
-    f = open('img.txt','w')
+    f = open('img.txt', 'w')
     f.write(image)
     f.close()
+
 
 '''
 method main():
@@ -83,9 +97,10 @@ method main():
 if __name__ == '__main__':
     import sys
     import urllib.request
+
     if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
         urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
-        path = "asciify.jpg"
+        input_image_path = "asciify.jpg"
     else:
-        path = sys.argv[1]
-    runner(path)
+        input_image_path = sys.argv[1]
+    runner(input_image_path)
